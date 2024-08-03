ef969d98a28cd039d9e421c063417e6a29cf8f83
Arun <arun.kallalsoma@education.nsw.gov.au>
2024-08-03 17:05:53 +1000

    Trying to make decision system

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool ask() {
+  bool done = true;
+
+  while (done) {
+    char ans;
+
+    printf("What will you do (a or b): ");
+    scanf("%c", ans);
+
+    if (tolower(ans) == "a") {
+      done = false;
+      return true;
+    }
+    else if (tolower(ans) == "b") {
+      done = false;
+      return false;
+    }
+    else {
+      done = true;
+      printf("Invalid response. Please try again");
+    }
+  }
+}
+
+int main() {
+  char *textA[] = {"Text", "Text2", "Text3"};
+  char *textB[] = {"TextB", "TextB2", "TextB3"};
+  printf("%d", ask());
+}
+
+
