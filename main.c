76ee247914c2d6c5b1368a7561780d43624967c1
Arun <arun.kallalsoma@education.nsw.gov.au>
2024-08-03 17:32:09 +1000

    Fixed decision system

--- main.c
+++ main.c
@@ -5,23 +5,23 @@
 bool ask() {
   bool done = true;
 
-  while (done) {
+  while (done == true) {
     char ans;
 
     printf("What will you do (a or b): ");
-    scanf("%c", ans);
+    scanf(" %c", &ans);
 
-    if (tolower(ans) == "a") {
+    if (tolower(ans) == *"a") {
       done = false;
       return true;
     }
-    else if (tolower(ans) == "b") {
+    else if (tolower(ans) == *"b") {
       done = false;
       return false;
     }
     else {
       done = true;
-      printf("Invalid response. Please try again");
+      printf("Invalid response. Please try again\n");
     }
   }
 }
