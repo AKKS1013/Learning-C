dec6dcd2efc033818163ac16315a07fa79ad1b09
Arun <arun.kallalsoma@education.nsw.gov.au>
2024-08-04 17:14:52 +1000

    Continued story

--- main.c
+++ main.c
@@ -52,7 +52,22 @@ int main() {
   if (ask("Would you like to:\nA - Run to the fight  OR  B - Find people to come and help\n")) {
     printf("You rush into the fight, attacking both friend and foe, until you see the source of the scream\n");
     printf("The scream had come from you brother, Irwin, who was lying on the floor bleeding profusely from the face\n");
-    ask("Do you:\nA - Run to your brother trying to help him  OR  B - run after the man who had hit him in the face, and was currently disappearing into the crowd\n");
+    if (ask("Do you:\nA - Run to your brother trying to help him  OR  B - run after the man who had hit him in the face, and was currently disappearing into the crowd\n")) {
+      printf("You sprint down to your brother's side, using your clothes to apply pressure and stop the bleeding\n");
+      printf("Beneath your clothes you can feel his breath steadying as you pick him up and start running out of the fight\n");
+      printf("You quickly spot the town doctor that was luckily nearby at that moment and sprint over to him carrying your unconscious brother\n");
+      printf("As soon as the doctor sees you with your brother he becons you over to the nearest house and starts adminstering first aid\n");
+      printf("As you see your brother slowly get better you decide there is no point in remaining here\n");
+      ask("Would you like to:\nA - Try and find the man who attacked your brother, in the fight  OR  B - Run around town, near the fight, looking for the man who attacked your brother");
+    }
+    else {
+      printf("You sprint off in the direction the man had run, pushing through the brawling people but do not see him\n");
+      printf("As you run back to your brother you see that he has fallen unconscious with a new fountain of blood splurting out of hisd mouth\n");
+      printf("You run to his side, feeling for a pulse but realise it was too late\n");
+      printf("\n");
+      printf("\n");
+      printf("GAME OVER\n");
+    }
   }  
   else {
     printf("Who run into the main room to find the house deserted\n");
