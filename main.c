0be6b1a10535a5dd04980604a11f4611cf3e83ed
Arun <arun.kallalsoma@education.nsw.gov.au>
2024-08-07 19:04:43 +1000

    Progressing with the hit/stand loop

--- main.c
+++ main.c
@@ -1,22 +1,56 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-srandom(unsigned int time());
-
 int draw() { return (random() % 13) + 1; }
 
+int more(int total) {
+  bool hit = true;
+  char ans;
+  printf("Would you like to (h)it or (s)tand: ");
+  scanf(" %c", &ans);
+
+  if (ans == 'h') {
+    hit = true;
+  } else {
+    hit = false;
+  }
+
+  while (hit) {
+    int card = draw();
+    total += card;
+    printf("Your card: %i\n", card);
+    if (total > 21) {
+      printf("Sorry, you BUST!");
+      return total;
+    }
+    printf("Would you like to (h)it or (s)tand: ");
+    scanf(" %c", &ans);
+
+    if (ans == 'h') {
+      hit = true;
+    } else {
+      hit = false;
+    }
+  }
+  return total;
+}
+
 int main(int argc, char *argv[]) {
+  srandom((unsigned int)time(NULL));
+
   printf("Welcome to CLI Blackjack");
   printf("------------------------");
 
-  int player_total = 0;
   int card1 = 0;
   int card2 = 0;
   card1 = draw();
   card2 = draw();
-  printf("Here are your 2 cards\n");
-  printf("%i and %i", card1, card2);
+  int total = card1 + card2;
+  printf("Here are your 2 cards:\n");
+  printf("%i and %i\n", card1, card2);
+  total = more(total);
 
   return EXIT_SUCCESS;
 }
