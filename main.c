81e503a8e798d3b71705f9c96fd18bb2651d660a
Arun <arun.kallalsoma@education.nsw.gov.au>
2024-08-24 16:23:15 +1000

    Why won't u add numbers anymore :(((

--- main.c
+++ main.c
@@ -3,51 +3,73 @@
 #include <gtk/gtk.h>
 #include <gtk/gtkshortcut.h>
 #include <stdio.h>
+#include <time.h>
 #include <wchar.h>
 
 char ops[] = {0, 0, 0};
-char ni = '9';
-char ei = '8';
-char se = '7';
-char si = '6';
-char fi = '5';
-char fo = '4';
-char th = '3';
-char tw = '2';
-char on = '1';
-char pl = '+';
-char mi = '-';
-char mu = '*';
-char di = '/';
+char *ni = "9";
+char *ei = "8";
+char *se = "7";
+char *si = "6";
+char *fi = "5";
+char *fo = "4";
+char *th = "3";
+char *tw = "2";
+char *on = "1";
+char *pl = "+";
+char *mi = "-";
+char *mu = "*";
+char *di = "/";
+volatile unsigned int *te = (volatile unsigned int *)0x5dede3e5d0dd;
 float ans = 0;
 char lab[32] = {0};
+char ans2[32] = {0};
+bool first = false;
+bool second = false;
 
 GtkWidget *anslabel;
 
-static void add_char(GtkWidget *widget, gpointer data) {
-  char *pn = data;
-  if (*pn != 0) {
-    ops[0] = *pn;
-  } else {
+static void add_char(GtkWidget *widget, gpointer user_data) {
+  char *pn = user_data;
+  if (*pn == '+') {
     ops[1] = *pn;
+  } else if (*pn == '-') {
+    ops[1] = *pn;
+  } else if (*pn == '*') {
+    ops[1] = *pn;
+  } else if (*pn == '/') {
+    ops[1] = *pn;
+  } else {
+    if (first && second) {
+      ops[0] = *pn;
+    } else if (first) {
+      ops[2] = *pn;
+      second = true;
+    } else {
+      ops[0] = *pn;
+      first = true;
+    }
   }
-  g_print(&ops[0]);
-  g_print(&ops[1]);
 }
 
-static void equals(GtkWidget *widget, gpointer data) {
-  switch (ops[1]) {
-  case '+':
+static void equals(GtkWidget *widget, gpointer user_data) {
+  g_print(ops);
+
+  if (ops[1] == '+') {
     ans = ops[0] + ops[2];
-  case '-':
+    g_snprintf(ans2, sizeof ans, "\n%f", ans);
+    g_print(ans2);
+  } else if (ops[1] == '-') {
     ans = ops[0] - ops[2];
-  case '*':
+  } else if (ops[1] == '*') {
     ans = ops[0] * ops[2];
-  case '/':
+  } else if (ops[1] == '/') {
     ans = (float)ops[0] / ops[2];
-  }
-  g_snprintf(lab, sizeof lab, "%f", ans);
+  } else
+
+    g_snprintf(lab, sizeof lab, "%f", ans);
   gtk_label_set_text(GTK_LABEL(anslabel), lab);
+  g_print("\n");
   g_print(lab);
 }
 
@@ -79,43 +101,43 @@ static void activate(GtkApplication *app, gpointer user_data) {
   gtk_window_set_child(GTK_WINDOW(window), grid);
 
   button9 = gtk_button_new_with_label("9");
-  g_signal_connect(button9, "clicked", G_CALLBACK(add_char), &ni);
+  g_signal_connect(button9, "clicked", G_CALLBACK(add_char), ni);
 
   button8 = gtk_button_new_with_label("8");
-  g_signal_connect(button8, "clicked", G_CALLBACK(add_char), &ei);
+  g_signal_connect(button8, "clicked", G_CALLBACK(add_char), ei);
 
   button7 = gtk_button_new_with_label("7");
-  g_signal_connect(button7, "clicked", G_CALLBACK(add_char), &se);
+  g_signal_connect(button7, "clicked", G_CALLBACK(add_char), se);
 
   button6 = gtk_button_new_with_label("6");
-  g_signal_connect(button6, "clicked", G_CALLBACK(add_char), &si);
+  g_signal_connect(button6, "clicked", G_CALLBACK(add_char), si);
 
   button5 = gtk_button_new_with_label("5");
-  g_signal_connect(button5, "clicked", G_CALLBACK(add_char), &fi);
+  g_signal_connect(button5, "clicked", G_CALLBACK(add_char), fi);
 
   button4 = gtk_button_new_with_label("4");
-  g_signal_connect(button4, "clicked", G_CALLBACK(add_char), &fo);
+  g_signal_connect(button4, "clicked", G_CALLBACK(add_char), fo);
 
   button3 = gtk_button_new_with_label("3");
-  g_signal_connect(button3, "clicked", G_CALLBACK(add_char), &th);
+  g_signal_connect(button3, "clicked", G_CALLBACK(add_char), th);
 
   button2 = gtk_button_new_with_label("2");
-  g_signal_connect(button2, "clicked", G_CALLBACK(add_char), &tw);
+  g_signal_connect(button2, "clicked", G_CALLBACK(add_char), tw);
 
   button1 = gtk_button_new_with_label("1");
-  g_signal_connect(button1, "clicked", G_CALLBACK(add_char), &on);
+  g_signal_connect(button1, "clicked", G_CALLBACK(add_char), on);
 
   plusbutton = gtk_button_new_with_label("+");
-  g_signal_connect(plusbutton, "clicked", G_CALLBACK(add_char), &pl);
+  g_signal_connect(plusbutton, "clicked", G_CALLBACK(add_char), pl);
 
   minusbutton = gtk_button_new_with_label("-");
-  g_signal_connect(minusbutton, "clicked", G_CALLBACK(add_char), &mi);
+  g_signal_connect(minusbutton, "clicked", G_CALLBACK(add_char), mi);
 
   multibutton = gtk_button_new_with_label("x");
-  g_signal_connect(multibutton, "clicked", G_CALLBACK(add_char), &mu);
+  g_signal_connect(multibutton, "clicked", G_CALLBACK(add_char), mu);
 
   divbutton = gtk_button_new_with_label("/");
-  g_signal_connect(multibutton, "clicked", G_CALLBACK(add_char), &di);
+  g_signal_connect(multibutton, "clicked", G_CALLBACK(add_char), di);
 
   eqbutton = gtk_button_new_with_label("=");
   g_signal_connect(eqbutton, "clicked", G_CALLBACK(equals), NULL);
@@ -150,6 +172,10 @@ int main(int argc, char **argv) {
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
+
+  printf("\n %c", ops[0]);
+  printf("\n %c", ops[1]);
+  printf("\n %c", ops[2]);
 
   return status;
 }
