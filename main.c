3bc48c63d8e063bd0721ec830377f805ef7be934
AKKS1013 <46385422+AKKS1013@users.noreply.github.com>
2024-09-04 19:19:34 +1000

    forgot miniscraps :( - scrap 1

--- main.c
+++ main.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main() {
   char op;
   double first, second;
-  bool quit = true;
 
   printf("Enter an operator (+, -, *, /): ");
   scanf("%c", &op);
 
   printf("Enter two operands: ");
-  scanf("%lf %lf", &first, &second);
+  scanf("%lf", &first);
+  scanf("%lf", &second);
 
-  switch (op) {
-    case '+':
-      printf("%.2lf + %.2lf = %.2lf\n", first, second, first + second);
-      break;
-    case '-':
-      printf("%.2lf - %.2lf = %.2lf\n", first, second, first - second);
-      break;
-    case '*':
-      printf("%.2lf * %.2lf = %.2lf\n", first, second, first * second);
-      break;
-    case '/':
-      printf("%.2lf / %.2lf = %.2lf\n", first, second, first / second);
-      break;
-  // operator doesn't match any case constant
-    default:
-      printf("Invalid operator entered. Please try again\n");
-  }
- }
+  printf("%lf ", first);
+  printf("%lf ", second);
+  printf("%c", op);
+}
